003d5b9138322d130be578656a6fd7ca29bfc97f
herrera_luis10 <herrera_luis10@hotmail.com>
2018-10-16 21:05:46 +0000

    updates script setup.sh

--- setup.sh
+++ setup.sh
@@ -8,5 +8,35 @@ else
     mkdir $(pwd)/artifactory
 fi
 
+if [ -d $(pwd)/artifactory/data ]
+then
+    echo "Directory artifactory/data exists."
+else
+    echo "Creating the directory artifactory/data."
+    mkdir $(pwd)/artifactory/data
+fi
+
+if [ -d $(pwd)/artifactory/backup ]
+then
+    echo "Directory artifactory/backup exists."
+else
+    echo "Creating the directory artifactory/backup."
+    mkdir $(pwd)/artifactory/backup
+fi
+
+if [ -d $(pwd)/artifactory/etc ]
+then
+    echo "Directory artifactory/etc exists."
+else
+    echo "Creating the directory artifactory/etc."
+    mkdir $(pwd)/artifactory/etc
+fi
+
+
 docker build -t machine/artifactory .
-docker run -d -p 8081:8081 --name server-artifactory -v $(pwd)/artifactory:/usr/local/artifactory -v $(pwd):/home/DevOps --restart always machine/artifactory
+
+docker run -d -p 8081:8081 --name server-artifactory -v $(pwd)/artifactory/data:/usr/local/artifactory/data \
+-v $(pwd)/artifactory/backup:/usr/local/artifactory/backup \
+-v $(pwd)/artifactory/etc:/usr/local/artifactory/etc \
+-v $(pwd):/home/DevOps \
+--restart always machine/artifactory
